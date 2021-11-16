a8ae0404aa64f541ec6408afa3573aba959109fb
afshinparhizkari <Afshin.Parhizkari@gmail.com>
2021-11-16 20:52:52 +0330

    Finance app client connect to Eureka for getting product/quantity address

--- Finance/src/main/java/com/afshin/finance/infrastructure/resource/ProductRso.java
+++ Finance/src/main/java/com/afshin/finance/infrastructure/resource/ProductRso.java
@@ -9,6 +9,7 @@ package com.afshin.finance.infrastructure.resource;
  * Description:
  */
 import com.afshin.finance.domain.entity.Quantity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -21,14 +22,16 @@ import java.util.List;
 
 @Component
 public class ProductRso {
-    RestTemplate restTemplate=new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
+	@Autowired RestTemplate restTemplate;
+	@Autowired ResorceConfig resoConfig;
+    @Value("${product.quantity}") private String serviceName;
 
     @Value("${product.quantity}") private String productPath;
     public List<Quantity> getQuantity(List<Integer> inputValue) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request =new HttpEntity<String>(inputValue.toString(), headers);
-        Quantity[] quantities =restTemplate.postForObject(productPath, request, Quantity[].class);
+        Quantity[] quantities =restTemplate.postForObject(resoConfig.getURI("product")+serviceName, request, Quantity[].class);
         //String productJson =restTemplate.postForObject(findPath, request, String.class);
         return Arrays.asList(quantities);
     }
