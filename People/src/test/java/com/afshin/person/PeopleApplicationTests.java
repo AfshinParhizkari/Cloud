d76ff91a6e976827c74f8859e6fcfdfbfecd0784
afshin.parhizkari@gmail.com <afshin.parhizkari@gmail.com>
2023-09-12 16:28:05 +0330

    change port

--- People/src/test/java/com/afshin/person/PeopleApplicationTests.java
+++ People/src/test/java/com/afshin/person/PeopleApplicationTests.java
@@ -1,7 +1,6 @@
 package com.afshin.person;
 
 import com.afshin.person.application.PeopleRst;
-import com.afshin.person.logger.MyLogger;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
